021cf1e3a7006f0920da2e8fade4bbde955de238
Dynocoder <sauravprashar21@gmail.com>
2025-02-16 20:18:23 -0500

    oil - show hidden files with g.

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,9 +1,17 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_hidden_file = function(name, bufnr)
+        local m = name:match '^%.'
+        return m ~= nil
+      end,
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    require('oil').setup {}
     vim.keymap.set('n', '<leader>e', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
 }
